c8068a45a6bdb2f1220ce077d83c6447fdb8e827
prakharparwal <prakhar.parwal@thoughtworks.com>
2022-04-24 14:22:59 +0530

    Created and added header component in the page

---
+++ src/components/VersedHeader.js
@@ -0,0 +1,14 @@
+import '../css/VersedHeader.css';
+
+function VersedHeader() {
+    return (
+        <div className="wrapper">
+            <div className = "versed-app-header">
+            Versed App
+            </div>
+        </div>
+        
+    );
+}
+
+export default VersedHeader;
